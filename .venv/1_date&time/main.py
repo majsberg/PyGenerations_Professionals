56439e375effd71fdc0f440aeb506e3b9bd4a0a0
majsberg <majsberg@yandex.ru>
2025-09-18 23:12:37 +0300

    test commit

--- .venv/1_date&time/main.py
+++ .venv/1_date&time/main.py
@@ -1,3 +1,12 @@
+# from datetime import date
+#
+# date_list = [date.fromisoformat(input()) for i in range(2)]
+# print(min(date_list).strftime("%d-%m (%Y)"))
+
+
 from datetime import date
 
-print(date.strftime(date.today(), '%B %d, %Y'))
+n = int(input())
+list_dates = [date.fromisoformat(input()) for _ in range(n)]
+for each in sorted(list_dates):
+    print(each.strftime("%d/%m/%Y"))
