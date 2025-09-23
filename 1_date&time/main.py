dd01d2cbf5398cd354962bb5e5006eb34d748aba
majsberg <majsberg@yandex.ru>
2025-09-23 14:35:18 +0300

    test commit

--- 1_date&time/main.py
+++ 1_date&time/main.py
@@ -93,18 +93,100 @@
 # list_after = list(filter(lambda x: x.hour >= 12, times_of_purchases))
 # print("До полудня") if len(list_before) > len(list_after) else print("После полудня")
 
-from datetime import date, time, datetime
+# from datetime import date, time, datetime
+#
+# dates = [date(1793, 8, 23), date(1410, 3, 11), date(804, 11, 12), date(632, 6, 4),
+#          date(295, 1, 23), date(327, 8, 24), date(167, 4, 16), date(229, 1, 24),
+#          date(1239, 2, 5), date(1957, 7, 14), date(197, 8, 24), date(479, 9, 6)]
+#
+# times = [time(7, 33, 27), time(21, 2, 10), time(17, 20, 47), time(20, 8, 59),
+#          time(12, 42, 56), time(15, 9, 57), time(17, 47, 9), time(9, 40, 2),
+#          time(11, 47, 1), time(17, 27, 10), time(17, 55, 40), time(9, 14, 9)]
+#
+# zip_list = list(zip(dates, times))
+# result_list = list(map(lambda x: datetime.combine(x[0], x[1]), zip_list))
+# result_list = sorted(result_list, key=lambda x: x.second)
+# for each in result_list:
+#     print(each)
 
-dates = [date(1793, 8, 23), date(1410, 3, 11), date(804, 11, 12), date(632, 6, 4),
-         date(295, 1, 23), date(327, 8, 24), date(167, 4, 16), date(229, 1, 24),
-         date(1239, 2, 5), date(1957, 7, 14), date(197, 8, 24), date(479, 9, 6)]
+# from datetime import datetime
+#
+# data = {'Дима': ('03.11.2021 09:31:18', '03.11.2021 11:41:28'),
+#         'Геор': ('01.11.2021 09:03:04', '01.11.2021 12:40:35'),
+#         'Анна': ('02.11.2021 04:41:54', '02.11.2021 05:39:40'),
+#         'Илина': ('02.11.2021 01:36:40', '02.11.2021 04:48:27'),
+#         'Герман': ('04.11.2021 07:51:19', '04.11.2021 09:53:53'),
+#         'Руслан': ('01.11.2021 11:26:06', '01.11.2021 12:56:24'),
+#         'Лера': ('03.11.2021 11:09:41', '03.11.2021 14:37:41'),
+#         'Егор': ('03.11.2021 05:29:38', '03.11.2021 06:01:59'),
+#         'Максим': ('05.11.2021 13:05:03', '05.11.2021 14:27:41'),
+#         'Саша': ('03.11.2021 04:14:26', '03.11.2021 05:10:58'),
+#         'Марина': ('05.11.2021 15:21:06', '05.11.2021 18:33:46')}
+#
+# list_ = [(i[0], datetime.strptime(i[1][1], '%d.%m.%Y %H:%M:%S') - datetime.strptime(i[1][0], '%d.%m.%Y %H:%M:%S')) for i in data.items()]
+# print(min(list_, key=lambda x: x[1])[0])
 
-times = [time(7, 33, 27), time(21, 2, 10), time(17, 20, 47), time(20, 8, 59),
-         time(12, 42, 56), time(15, 9, 57), time(17, 47, 9), time(9, 40, 2),
-         time(11, 47, 1), time(17, 27, 10), time(17, 55, 40), time(9, 14, 9)]
+# from datetime import datetime
+#
+# with open("files/diary.txt", encoding="UTF-8") as file:
+#     list_ = file.read().split('\n')
+#     main_list = list()
+#     inner_list = list()
+#
+#     for each in list_:
+#         if each != '':
+#             inner_list.append(each)
+#         elif each == '':
+#             main_list.append(inner_list.copy())
+#             inner_list.clear()
+#     main_list.append(inner_list.copy())
+#
+#     main_list = list(map(lambda x: (datetime.strptime(x[0], "%d.%m.%Y; %H:%M"), *x[1:]), main_list))
+#
+#     main_list.sort(key=lambda x: x[0])
+#     for i in range(len(main_list)):
+#         for string in main_list[i]:
+#             if type(string) == datetime:
+#                 print(string.strftime("%d.%m.%Y; %H:%M"))
+#             else:
+#                 print(string)
+#         print() if i < len(main_list) - 1 else None
 
-zip_list = list(zip(dates, times))
-result_list = list(map(lambda x: datetime.combine(x[0], x[1]), zip_list))
-result_list = sorted(result_list, key=lambda x: x.second)
-for each in result_list:
-    print(each)
+from datetime import datetime
+
+def is_available_date(booked_dates, date_for_booking):
+    reserved_dates = list()
+    required_dates = list()
+
+    def check_dates(date_string):
+        list_dates = date_string.split('-')
+        list_dates = list(map(lambda x: datetime.strptime(x, '%d.%m.%Y'), list_dates))
+        return list(map(lambda x: x.toordinal(), list_dates))
+
+    for date_string in booked_dates:
+        if len(date_string) == 10:
+            date_ = datetime.strptime(date_string, '%d.%m.%Y')
+            reserved_dates.append(date_.toordinal())
+        else:
+            list_dates = check_dates(date_string)
+            reserved_dates.extend(list(range(list_dates[0], list_dates[-1] + 1)))
+
+    if len(date_for_booking) == 10:
+        required_dates.append(datetime.strptime(date_for_booking, '%d.%m.%Y').toordinal())
+    else:
+        list_dates = check_dates(date_for_booking)
+        required_dates.extend(list(range(list_dates[0], list_dates[-1] + 1)))
+
+    print(reserved_dates)
+    print(required_dates)
+
+    set_intersection = set(reserved_dates).intersection(required_dates)
+    if len(set_intersection) > 0:
+        return False
+    else:
+        return True
+
+
+dates = ['01.11.2021', '05.11.2021-09.11.2021', '12.11.2021', '15.11.2021-21.11.2021']
+some_date = '22.11.2021-25.11.2021'
+print(is_available_date(dates, some_date))
